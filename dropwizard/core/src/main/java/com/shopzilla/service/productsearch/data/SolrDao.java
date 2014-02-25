95c21dcb19bc223b416101c7a68aaf3e61977eaa
Brett Konold <brettkonold@gmail.com>
2014-02-24 20:43:30 -0800

    Implemented retrieving Solr results

--- dropwizard/core/src/main/java/com/shopzilla/service/productsearch/data/SolrDao.java
+++ dropwizard/core/src/main/java/com/shopzilla/service/productsearch/data/SolrDao.java
@@ -1,23 +1,47 @@
 package com.shopzilla.service.productsearch.data;
 
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+
+import java.util.List;
+
 /**
  * Created by brett on 2/23/14.
  * TODO: issue query to solr, map solr response into list of SolrProductEntry
  */
 public class SolrDao {
 
-    private String solrBaseUrl;
+    public static void main(String[] args) throws Exception {
+        SolrDao dao = new SolrDao("http://localhost:8983/solr/collection1");
+        List<SolrProductEntry> products = dao.getSearchResults("apple");
+        for (SolrProductEntry p : products) {
+            System.out.println(p.getDisplayName());
+        }
+    }
+
+    private SolrServer solrServer;
+    private static final String requestHandler = "/simpleweighted";
 
     public SolrDao(String solrBaseUrl) {
-        this.solrBaseUrl = solrBaseUrl;
+        this.solrServer = new HttpSolrServer(solrBaseUrl);
     }
 
-    public String getSolrBaseUrl() {
-        return solrBaseUrl;
+    public SolrServer getSolrServer() {
+        return solrServer;
     }
 
-    public void setSolrBaseUrl(String solrBaseUrl) {
-        this.solrBaseUrl = solrBaseUrl;
+    public void setSolrServer(SolrServer solrServer) {
+        this.solrServer = solrServer;
     }
 
+    public List<SolrProductEntry> getSearchResults(String query) throws Exception {
+        if (query == null || query.length() < 1)
+            return null; // TODO: throw exception?
+
+        SolrQuery solrQuery = new SolrQuery().setQuery(query).setRequestHandler(requestHandler);
+        QueryResponse response = solrServer.query(solrQuery);
+        return response.getBeans(SolrProductEntry.class);
+    }
 }
--- dropwizard/core/src/main/java/com/shopzilla/service/productsearch/data/SolrProductEntry.java
+++ dropwizard/core/src/main/java/com/shopzilla/service/productsearch/data/SolrProductEntry.java
@@ -1,9 +1,94 @@
 package com.shopzilla.service.productsearch.data;
 
+import org.apache.solr.client.solrj.beans.Field;
+
+import java.util.List;
+
 /**
  * Created by brett on 2/23/14.
  */
 public class SolrProductEntry {
-
     //TODO: define elements that are in the solr response
+
+    private String pid;
+    private String category;
+    private String displayName;
+    private String brand;
+    private String title;
+    private List<String> reviewRatings;
+    private List<String> reviewTitles;
+    private List<String> reviewContents;
+
+    public String getPid() {
+        return pid;
+    }
+
+    @Field("PID")
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    @Field("Category")
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Field("DisplayName")
+    public void setDisplayName(String name) {
+        this.displayName = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    @Field("Brand")
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Field("ProductTitle")
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getReviewRatings() {
+        return reviewRatings;
+    }
+
+    @Field("Rating")
+    public void setReviewRatings(List<String> reviewRatings) {
+        this.reviewRatings = reviewRatings;
+    }
+
+    public List<String> getReviewTitles() {
+        return reviewTitles;
+    }
+
+    @Field("ReviewTitle")
+    public void setReviewTitles(List<String> reviewTitles) {
+        this.reviewTitles = reviewTitles;
+    }
+
+    public List<String> getReviewContents() {
+        return reviewContents;
+    }
+
+    @Field("Review")
+    public void setReviewContents(List<String> reviewContents) {
+        this.reviewContents = reviewContents;
+    }
+
 }
