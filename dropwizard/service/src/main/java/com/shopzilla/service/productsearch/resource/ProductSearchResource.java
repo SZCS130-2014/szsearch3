c5485f4c7032537d4052131cc8499561b37d35eb
Brett Konold <brettkonold@gmail.com>
2014-02-26 21:25:14 -0800

    Implemented ProductSearchResource.

--- dropwizard/service/src/main/java/com/shopzilla/service/productsearch/resource/ProductSearchResource.java
+++ dropwizard/service/src/main/java/com/shopzilla/service/productsearch/resource/ProductSearchResource.java
@@ -7,6 +7,7 @@ package com.shopzilla.service.productsearch.resource;
 import com.fasterxml.jackson.jaxrs.json.annotation.JSONP;
 import com.shopzilla.service.productsearch.Format;
 import com.shopzilla.service.productsearch.data.SolrDao;
+import com.shopzilla.service.productsearch.data.SolrProductEntry;
 import com.shopzilla.site.service.productsearch.model.jaxb.ProductSearchResponse;
 import com.shopzilla.site.service.productsearch.model.jaxb.ProductEntry;
 import com.shopzilla.site.service.productsearch.model.jaxb.CommentEntry;
@@ -23,17 +24,16 @@ import javax.ws.rs.core.Response;
 import java.util.List;
 
 /**
- * TODO: a lot
  * @author Brett Konold
  */
 @Path("/productsearch")
 @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 public class ProductSearchResource {
 
-    private SolrDao dao;
+    private SolrDao solrDao;
 
     public ProductSearchResource(SolrDao dao) {
-        this.dao = dao;
+        this.solrDao = dao;
     }
 
     @GET
@@ -41,14 +41,38 @@ public class ProductSearchResource {
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     //@Path("")
     public Response get(@QueryParam("q") String query,
-                        @QueryParam("format") Format format) {
+                        @QueryParam("format") Format format) throws Exception {
         if (query == null || query.length() < 1) {
             // TODO: log?
             return Response.status(Response.Status.NOT_ACCEPTABLE).build();
         }
 
         ProductSearchResponse response = new ProductSearchResponse();
-        // TODO: map from SolrProductEntry to ProductEntry
+
+        List<SolrProductEntry> solrProductEntries = solrDao.getSearchResults(query);
+        for (SolrProductEntry solrProductEntry : solrProductEntries) {
+            ProductEntry productEntry = new ProductEntry();
+            // TODO: make sure all PIDs are valid longs?
+            productEntry.setPid(Long.parseLong(solrProductEntry.getPid()));
+            productEntry.setBrand(solrProductEntry.getBrand());
+            productEntry.setCategory(solrProductEntry.getCategory());
+            productEntry.setName(solrProductEntry.getDisplayName());
+            productEntry.setTitle(solrProductEntry.getTitle());
+
+            int length = solrProductEntry.getReviewRatings().size();
+
+            for (int i = 0; i < length; i++) {
+                CommentEntry commentEntry = new CommentEntry();
+                commentEntry.setContent(solrProductEntry.getReviewContents().get(i));
+                commentEntry.setTitle(solrProductEntry.getReviewTitles().get(i));
+                String strRating = solrProductEntry.getReviewRatings().get(i);
+                double rating = (strRating == null || strRating.equals("")) ? 0 : Double.parseDouble(strRating);
+                commentEntry.setRating(rating);
+
+                productEntry.getCommentEntry().add(commentEntry);
+            }
+            response.getProductEntry().add(productEntry);
+        }
         return buildResponse(response, format);
     }
 
@@ -59,11 +83,11 @@ public class ProductSearchResource {
     }
 
     public SolrDao getDao() {
-        return dao;
+        return solrDao;
     }
 
     public void setDao(SolrDao dao) {
-        this.dao = dao;
+        this.solrDao = dao;
     }
 
 }
