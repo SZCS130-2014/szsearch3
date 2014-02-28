3f43b5f9bd4861e6b7baf98a3dd5eba0ce89f74e
Josh-Dykstra <joshuajdykstra@gmail.com>
2014-02-28 12:49:31 -0800

    Added functionality to fetch search results from the service
    * Product model accesses service for results with HTTParty

--- web-app/app/models/product.rb
+++ web-app/app/models/product.rb
@@ -3,7 +3,39 @@
 # A model for managing products
 # Author: Joshua Dykstra
 # Date: 2/27/14
-class Product < ActiveRecord::Base
+class Product 
 
+	require 'rubygems'
+	require 'json'
+	include HTTParty
+
+	base_uri '54.213.229.102:7500/services/'
+
+	#
+	# Issues a query to the search service
+	# param: keywords - words to search
+	# optional param: start - offset to being returning results
+	# optional param: rows - number of results ro return
+	# return object containing 
+	#
+	def self.search(keywords, start = 0, rows = 20)
+
+		# Check if we don't have any keywords
+		if keywords.nil?
+			return nil
+		end
+
+		# Issue a query to the service using HTTParty
+		params = { :query => {:q => keywords, :start => start,
+							  :rows => rows, :format => 'json'} }
+		response = get("#{@base_uri}/productsearch", params)
+
+		if response.success?
+			return JSON.parse(response.body)
+		else
+			raise response.response
+		end
+
+	end
 
 end
